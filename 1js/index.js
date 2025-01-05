ff3f66cc69025732292314fe01495dcd3847e4dc
Eusebiu Vlad Hidis <Eusebiu.Hidis@zucchetti.com>
2025-01-05 17:16:21 +0200

    v1

---
+++ 1js/index.js
@@ -0,0 +1,15 @@
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var topnav=document.querySelector(".topnav");
+    var x = document.getElementById("myLinks");
+    var icon=document.querySelector(".logo");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+      topnav.style.height="20%";
+    } else {
+      x.style.display = "block";
+      topnav.style.height="auto";
+      
+    }
+  }
+   
