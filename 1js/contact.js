ac952a090306924a2b806517809d7f66514884ab
Eusebiu Vlad Hidis <Eusebiu.Hidis@zucchetti.com>
2025-01-15 10:07:04 +0200

    hamburger menu functional

--- 1js/contact.js
+++ 1js/contact.js
@@ -17,3 +17,16 @@ form.addEventListener("submit", (e)=>{
     e.preventDefault();
     sendMail();
 })
+function myFunction() {
+    var topnav=document.querySelector(".topnav");
+    var x = document.getElementById("myLinks");
+    var icon=document.querySelector(".logo");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+      topnav.style.height="20%";
+    } else {
+      x.style.display = "block";
+      topnav.style.height="auto";
+      
+    }
+  }
